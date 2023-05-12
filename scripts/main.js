fa1b2564043f8cb5cf178c104aeecf34bb8d0ba8
Ryan Phillips <ryanmphill@gmail.com>
2023-05-12 13:33:18 -0500

    Added button to save submissions with event listener to POST to API

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { OwnJeanChoices } from "./OwnJeans.js"
+import { SaveSubmission } from "./SaveSubmission.js"
 import { LocationTypeChoices } from "./UrbanDweller.js"
 
 const container = document.querySelector("#mainContainer")
@@ -6,10 +7,12 @@ const container = document.querySelector("#mainContainer")
 const render = async () => {
     const jeanOwnershipHTML = OwnJeanChoices()
     const locationsHTML = await LocationTypeChoices()
+    const buttonHTML = await SaveSubmission()
 
     container.innerHTML = `
         ${jeanOwnershipHTML}
         ${locationsHTML}
+        ${buttonHTML}
     `
 }
 
