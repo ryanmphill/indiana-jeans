c1c18ff4f01ef41ad1928c8d68d73e303fdac20b
Ryan Phillips <ryanmphill@gmail.com>
2023-05-12 11:19:44 -0500

    Added event listeners in OwnJeans and UrbanDwellers components and created new component to capture transient state changes

--- scripts/UrbanDweller.js
+++ scripts/UrbanDweller.js
@@ -1,4 +1,27 @@
+import { setSocioLocationId } from "./TransientState.js"
+
+
+ /*
+     Define the callback function for an event listener that targets a 'change' 
+     when the user chooses a location
+
+     Since the value of the radio buttons is the string of
+     integers, you must convert the
+     string to an actual integer with parseInt() as seen below
+ */
+     const handleLocationChange = (changeEvent) => {
+        if (changeEvent.target.name === "location") {
+            const convertedToInteger = parseInt(changeEvent.target.value)
+            setSocioLocationId(convertedToInteger)
+        }
+    }
+
+/*Export a function that invokes the event listener, fetches location data from api,
+  and builds the HTML for the location buttons */
 export const LocationTypeChoices = async () => {
+
+    document.addEventListener("change", handleLocationChange)
+
     const response = await fetch("http://localhost:8088/socioLocations")
     const locations = await response.json()
 
